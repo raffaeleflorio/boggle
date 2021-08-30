6a3df74a9adab025bbce79ef281eb23bffe25882
Raffaele Florio <raffaeleflorio@protonmail.com>
2021-08-30 20:12:56 +0200

    boggle package-info.java

---
+++ src/main/java/io/github/raffaeleflorio/boggle/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Objects about boggle game
+ *
+ * @author Raffaele Florio (raffaeleflorio@protonmail.com)
+ * @see <a href="https://en.wikipedia.org/wiki/Boggle">Wikipedia definition</a>
+ * @since 1.0.0
+ */
+package io.github.raffaeleflorio.boggle;
