7eac0cb03cb6eae15d8ac1e0f4a21a6cd9039c29
Raffaele Florio <raffaeleflorio@protonmail.com>
2021-08-30 20:14:22 +0200

    Die.Fake(Test)

--- src/main/java/io/github/raffaeleflorio/boggle/dice/Die.java
+++ src/main/java/io/github/raffaeleflorio/boggle/dice/Die.java
@@ -1,5 +1,7 @@
 package io.github.raffaeleflorio.boggle.dice;
 
+import java.util.function.Function;
+
 /**
  * Rollable object with marked sides
  *
@@ -23,4 +25,48 @@ public interface Die<T> {
    * @since 1.0.0
    */
   Die<T> rolled();
+
+  /**
+   * A {@link Die} useful for testing purpose
+   *
+   * @param <T> The mark side type
+   * @author Raffaele Florio (raffaeleflorio@protonmail.com)
+   * @since 1.0.0
+   */
+  final class Fake<T> implements Die<T> {
+    /**
+     * Builds a constant fake
+     *
+     * @param value The value
+     * @since 1.0.0
+     */
+    public Fake(final T value) {
+      this(value, Function.identity());
+    }
+
+    /**
+     * Builds a fake
+     *
+     * @param value  The value
+     * @param nextFn The function to build the next value
+     * @since 1.0.0
+     */
+    public Fake(final T value, final Function<T, T> nextFn) {
+      this.value = value;
+      this.nextFn = nextFn;
+    }
+
+    @Override
+    public T value() {
+      return value;
+    }
+
+    @Override
+    public Die<T> rolled() {
+      return new Die.Fake<>(nextFn.apply(value), nextFn);
+    }
+
+    private final T value;
+    private final Function<T, T> nextFn;
+  }
 }
