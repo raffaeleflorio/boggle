3cbc30b02513bd661b7af2be825fa5257bf337bb
Raffaele Florio <raffaeleflorio@protonmail.com>
2021-08-30 20:14:41 +0200

    AlignedDice(Test)

---
+++ src/main/java/io/github/raffaeleflorio/boggle/dice/AlignedDice.java
@@ -0,0 +1,41 @@
+package io.github.raffaeleflorio.boggle.dice;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+/**
+ * Aligned {@link Dice}, it preserves {@link Die} ordering
+ *
+ * @param <T> The mark side type
+ * @author Raffaele Florio (raffaeleflorio@protonmail.com)
+ * @since 1.0.0
+ */
+final class AlignedDice<T> implements Dice<T> {
+  /**
+   * Builds an aligned dice
+   *
+   * @param dice The dice
+   * @since 1.0.0
+   */
+  AlignedDice(final List<Die<T>> dice) {
+    this.dice = dice;
+  }
+
+  @Override
+  public Collection<T> values() {
+    return mappedDice(Die::value);
+  }
+
+  private <D> List<D> mappedDice(final Function<Die<T>, D> map) {
+    return dice.stream().map(map).collect(Collectors.toUnmodifiableList());
+  }
+
+  @Override
+  public Dice<T> shuffled() {
+    return new AlignedDice<>(mappedDice(Die::rolled));
+  }
+
+  private final List<Die<T>> dice;
+}
