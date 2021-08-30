1d188c79bc58ae5b3cb22496f30af31dd2502f9e
Raffaele Florio <raffaeleflorio@protonmail.com>
2021-08-30 20:39:17 +0200

    Dice.Fake(Test)

--- src/main/java/io/github/raffaeleflorio/boggle/dice/Dice.java
+++ src/main/java/io/github/raffaeleflorio/boggle/dice/Dice.java
@@ -1,6 +1,7 @@
 package io.github.raffaeleflorio.boggle.dice;
 
 import java.util.Collection;
+import java.util.function.Function;
 
 /**
  * A group of {@link Die}
@@ -25,4 +26,48 @@ public interface Dice<T> {
    * @since 1.0.0
    */
   Dice<T> shuffled();
+
+  /**
+   * A {@link Dice} useful for testing purpose
+   *
+   * @param <T> The mark side type
+   * @author Raffaele Florio (raffaeleflorio@protonmail.com)
+   * @since 1.0.0
+   */
+  final class Fake<T> implements Dice<T> {
+    /**
+     * Builds a fake
+     *
+     * @param values The values
+     * @since 1.0.0
+     */
+    public Fake(final Collection<T> values) {
+      this(values, Function.identity());
+    }
+
+    /**
+     * Builds a fake
+     *
+     * @param values The values
+     * @param nextFn The function used to builds the next value
+     * @since 1.0.0
+     */
+    public Fake(final Collection<T> values, final Function<Collection<T>, Collection<T>> nextFn) {
+      this.values = values;
+      this.nextFn = nextFn;
+    }
+
+    @Override
+    public Collection<T> values() {
+      return values;
+    }
+
+    @Override
+    public Dice<T> shuffled() {
+      return new Dice.Fake<>(nextFn.apply(values), nextFn);
+    }
+
+    private final Collection<T> values;
+    private final Function<Collection<T>, Collection<T>> nextFn;
+  }
 }
