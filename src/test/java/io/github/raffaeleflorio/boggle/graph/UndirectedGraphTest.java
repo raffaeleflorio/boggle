684fcbc78b879fc78f8e55cd62684f87ef123d77
Raffaele Florio <raffaeleflorio@protonmail.com>
2021-09-01 18:50:02 +0200

    UndirectedGraphTest improved

--- src/test/java/io/github/raffaeleflorio/boggle/graph/UndirectedGraphTest.java
+++ src/test/java/io/github/raffaeleflorio/boggle/graph/UndirectedGraphTest.java
@@ -7,23 +7,45 @@ import static org.hamcrest.Matchers.equalTo;
 
 class UndirectedGraphTest {
   @Test
-  void testConnection() {
+  void testUndirectedConnection() {
     assertThat(
       new UndirectedGraph<>()
         .edge(1, 2)
         .edge(2, 3)
         .edge(3, 4)
-        .connected(3, 4),
+        .connected(4, 3),
       equalTo(true)
     );
   }
 
   @Test
-  void testAdjacency() {
+  void testDirectedConnection() {
+    assertThat(
+      new UndirectedGraph<>()
+        .edge(1, 2)
+        .edge(2, 3)
+        .connected(2, 3),
+      equalTo(true)
+    );
+  }
+
+  @Test
+  void testUndirectedAdjacency() {
     assertThat(
       new UndirectedGraph<>()
         .edge(1, 2)
         .adjacent(2, 1),
+      equalTo(true)
+    );
+  }
+
+  @Test
+  void testDirectedAdjacency() {
+    assertThat(
+      new UndirectedGraph<>()
+        .edge(1, 2)
+        .edge(42, -42)
+        .adjacent(42, -42),
       equalTo(true)
     );
   }
