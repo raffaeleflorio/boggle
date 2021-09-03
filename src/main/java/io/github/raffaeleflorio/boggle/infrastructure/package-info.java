58560a7b3c94aed87b0eb4bfad5c21864deffc6a
Raffaele Florio <raffaeleflorio@protonmail.com>
2021-09-03 19:49:11 +0200

    Revert "Initial HTTP infrastructure implementation with vert.x and mutiny"
    This reverts commit 010b0bd2ac31b72d9047ea0bc46c9cef1e6e4501.

--- src/main/java/io/github/raffaeleflorio/boggle/infrastructure/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * Objects about boggle infrastructure
- *
- * @author Raffaele Florio (raffaeleflorio@protonmail.com)
- * @since 1.0.0
- */
-package io.github.raffaeleflorio.boggle.infrastructure;
