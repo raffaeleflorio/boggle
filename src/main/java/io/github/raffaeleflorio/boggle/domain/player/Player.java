abc38036b43cf57a03e82a3c2422623359199b62
Raffaele Florio <raffaeleflorio@protonmail.com>
2021-09-04 11:30:17 +0200

    Player.Fake(Test)

--- src/main/java/io/github/raffaeleflorio/boggle/domain/player/Player.java
+++ src/main/java/io/github/raffaeleflorio/boggle/domain/player/Player.java
@@ -29,4 +29,56 @@ public interface Player<T> {
    * @since 1.0.0
    */
   Uni<Sheet<T>> sheet(UUID id);
+
+  /**
+   * {@link Player} useful for testing
+   *
+   * @param <T> The word type
+   * @author Raffaele Florio (raffaeleflorio@protonmail.com)
+   * @since 1.0.0
+   */
+  final class Fake<T> implements Player<T> {
+    /**
+     * Builds a fake without sheets and with random id
+     *
+     * @since 1.0.0
+     */
+    public Fake() {
+      this(UUID.randomUUID());
+    }
+
+    /**
+     * Builds a fake without sheets
+     *
+     * @since 1.0.0
+     */
+    public Fake(final UUID id) {
+      this(id, Uni.createFrom().nullItem());
+    }
+
+    /**
+     * Builds a fake
+     *
+     * @param id    The id
+     * @param sheet The sheet
+     * @since 1.0.0
+     */
+    public Fake(final UUID id, final Uni<Sheet<T>> sheet) {
+      this.id = id;
+      this.sheet = sheet;
+    }
+
+    @Override
+    public UUID id() {
+      return id;
+    }
+
+    @Override
+    public Uni<Sheet<T>> sheet(final UUID id) {
+      return sheet;
+    }
+
+    private final UUID id;
+    private final Uni<Sheet<T>> sheet;
+  }
 }
---
+++ src/test/java/io/github/raffaeleflorio/boggle/domain/player/PlayerTest.java
@@ -0,0 +1,43 @@
+package io.github.raffaeleflorio.boggle.domain.player;
+
+import io.github.raffaeleflorio.boggle.domain.sheet.Sheet;
+import io.smallrye.mutiny.Uni;
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+import static io.github.raffaeleflorio.boggle.hamcrest.IsEmitted.emits;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.nullValue;
+
+class PlayerTest {
+  @Test
+  void testId() {
+    var expected = UUID.fromString("6b47aefc-85e6-4125-b930-dd15e9227377");
+    assertThat(
+      new Player.Fake<>(expected).id(),
+      equalTo(expected)
+    );
+  }
+
+  @Test
+  void testSheet() {
+    assertThat(
+      new Player.Fake<>().sheet(UUID.randomUUID()),
+      emits(nullValue())
+    );
+  }
+
+  @Test
+  void testCustomSheet() {
+    var expected = UUID.fromString("6b47aefc-85e6-4125-b930-dd15e9227377");
+    assertThat(
+      new Player.Fake<>(
+        UUID.randomUUID(),
+        Uni.createFrom().item(new Sheet.Fake<>(expected))
+      ).sheet(UUID.randomUUID()).onItem().transform(Sheet::id),
+      emits(equalTo(expected))
+    );
+  }
+}
