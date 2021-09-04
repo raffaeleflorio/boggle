ea042edaee4d8fb4764e7228f4340e23eba374f2
Raffaele Florio <raffaeleflorio@protonmail.com>
2021-09-04 11:28:23 +0200

    IsEmitted improved

--- src/test/java/io/github/raffaeleflorio/boggle/hamcrest/IsEmitted.java
+++ src/test/java/io/github/raffaeleflorio/boggle/hamcrest/IsEmitted.java
@@ -8,7 +8,7 @@ import org.hamcrest.TypeSafeDiagnosingMatcher;
 import java.time.Duration;
 
 public final class IsEmitted<T> extends TypeSafeDiagnosingMatcher<Uni<T>> {
-  public IsEmitted(final Matcher<T> origin, final Duration duration) {
+  public IsEmitted(final Matcher<? super T> origin, final Duration duration) {
     this.origin = origin;
     this.duration = duration;
   }
@@ -28,14 +28,14 @@ public final class IsEmitted<T> extends TypeSafeDiagnosingMatcher<Uni<T>> {
     origin.describeTo(description);
   }
 
-  public static <T> Matcher<Uni<T>> emits(final Matcher<T> matcher) {
+  public static <T> Matcher<Uni<T>> emits(final Matcher<? super T> matcher) {
     return new IsEmitted<>(matcher, Duration.ofMillis(100));
   }
 
-  public static <T> Matcher<Uni<T>> emits(final Matcher<T> matcher, final Duration duration) {
+  public static <T> Matcher<Uni<T>> emits(final Matcher<? super T> matcher, final Duration duration) {
     return new IsEmitted<>(matcher, duration);
   }
 
-  private final Matcher<T> origin;
+  private final Matcher<? super T> origin;
   private final Duration duration;
 }
