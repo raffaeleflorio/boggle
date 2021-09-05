7b7294ff1215a9ac2d37556fda87e6e58d57e2de
Raffaele Florio <raffaeleflorio@protonmail.com>
2021-09-05 12:14:38 +0200

    Matches.Fake(Test)

--- src/main/java/io/github/raffaeleflorio/boggle/domain/match/Matches.java
+++ src/main/java/io/github/raffaeleflorio/boggle/domain/match/Matches.java
@@ -4,6 +4,7 @@ import io.github.raffaeleflorio.boggle.domain.description.Description;
 import io.smallrye.mutiny.Uni;
 
 import java.util.UUID;
+import java.util.function.Function;
 
 /**
  * {@link Match} repository
@@ -29,4 +30,38 @@ public interface Matches<T> {
    * @since 1.0.0
    */
   Uni<Match<T>> match(UUID id);
+
+  /**
+   * {@link Matches} useful for testing
+   *
+   * @param <T> The word type
+   * @author Raffaele Florio (raffaeleflorio@protonmail.com)
+   * @since 1.0.0
+   */
+  final class Fake<T> implements Matches<T> {
+    /**
+     * Builds a fake
+     *
+     * @param newMatchFn The function to build new match
+     * @param matchFn    The function to build existing match
+     * @since 1.0.0
+     */
+    public Fake(final Function<Description, Uni<Match<T>>> newMatchFn, final Function<UUID, Uni<Match<T>>> matchFn) {
+      this.newMatchFn = newMatchFn;
+      this.matchFn = matchFn;
+    }
+
+    @Override
+    public Uni<Match<T>> match(final Description description) {
+      return newMatchFn.apply(description);
+    }
+
+    @Override
+    public Uni<Match<T>> match(final UUID id) {
+      return matchFn.apply(id);
+    }
+
+    private final Function<Description, Uni<Match<T>>> newMatchFn;
+    private final Function<UUID, Uni<Match<T>>> matchFn;
+  }
 }
