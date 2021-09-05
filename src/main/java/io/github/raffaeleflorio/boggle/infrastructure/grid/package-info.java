6fde8c35b41240974d116666d973811c1341a297
Raffaele Florio <raffaeleflorio@protonmail.com>
2021-09-05 09:40:06 +0200

    package-info infrastructure.grid

---
+++ src/main/java/io/github/raffaeleflorio/boggle/infrastructure/grid/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Infrastructure implementations about grids
+ *
+ * @author Raffaele Florio (raffaeleflorio@protonmail.com)
+ * @since 1.0.0
+ */
+package io.github.raffaeleflorio.boggle.infrastructure.grid;
