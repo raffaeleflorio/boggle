87baa1394a3d7d36cf710ff71486457766c3a498
Raffaele Florio <raffaeleflorio@protonmail.com>
2021-09-08 12:13:06 +0200

    HttpInfrastructure draft with createMatch and getMatch

--- src/main/java/io/github/raffaeleflorio/boggle/infrastructure/HttpInfrastructure.java
+++ src/main/java/io/github/raffaeleflorio/boggle/infrastructure/HttpInfrastructure.java
@@ -1,9 +1,9 @@
 package io.github.raffaeleflorio.boggle.infrastructure;
 
+import io.github.raffaeleflorio.boggle.domain.match.Match;
 import io.github.raffaeleflorio.boggle.domain.match.Matches;
 import io.github.raffaeleflorio.boggle.infrastructure.description.JsonAsDescription;
 import io.github.raffaeleflorio.boggle.infrastructure.match.MatchAsJson;
-import io.github.raffaeleflorio.boggle.infrastructure.sheet.SheetAsJson;
 import io.smallrye.mutiny.Uni;
 import io.smallrye.mutiny.vertx.core.AbstractVerticle;
 import io.vertx.core.json.JsonObject;
@@ -60,28 +60,26 @@ final class HttpInfrastructure extends AbstractVerticle {
   }
 
   private RouterBuilder binded(final RouterBuilder routerBuilder) {
-    routerBuilder.operation("buildSheet").handler(ctx -> {
-      var player = UUID.fromString(ctx.getBodyAsJson().getString("id"));
-      matches
-        .match(UUID.fromString(ctx.pathParam("match")))
-        .onItem().ifNotNull().transformToUni(match -> match.sheet(player))
-        .onItem().ifNotNull().transform(SheetAsJson::new)
-        .onItem().ifNotNull().transform(JsonObject::encodePrettily)
-        .onItem().ifNotNull().<Runnable>transform(json -> () -> ctx.response().setStatusCode(200).endAndForget(json))
-        .onItem().ifNull().continueWith(() -> ctx.response().setStatusCode(404).endAndForget())
-        .subscribe().with(Runnable::run);
-    });
-    routerBuilder.operation("createMatch").handler(ctx -> {
-      var description = new JsonAsDescription(ctx.getBodyAsJson());
-      matches
-        .match(description)
-        .onItem().ifNotNull().transform(MatchAsJson::new)
-        .onItem().ifNotNull().transform(JsonObject::encodePrettily)
-        .onItem().ifNotNull().<Runnable>transform(json -> () -> ctx.response().setStatusCode(200).endAndForget(json))
-        .onItem().ifNull().continueWith(() -> ctx.response().setStatusCode(404).endAndForget())
-        .subscribe().with(Runnable::run);
-    });
+    routerBuilder.operation("createMatch").handler(this::createMatch);
+    routerBuilder.operation("getMatch").handler(this::getMatch);
     return routerBuilder;
+  }
+
+  private void createMatch(final RoutingContext ctx) {
+    reply(ctx, matches.match(new JsonAsDescription(ctx.getBodyAsJson())));
+  }
+
+  private void reply(final RoutingContext ctx, final Uni<Match<CharSequence>> match) {
+    match
+      .onItem().ifNotNull().transformToUni(MatchAsJson::new)
+      .onItem().ifNotNull().transform(JsonObject::encodePrettily)
+      .onItem().ifNotNull().<Runnable>transform(json -> () -> ctx.response().setStatusCode(200).endAndForget(json))
+      .onItem().ifNull().continueWith(() -> ctx.response().setStatusCode(404).endAndForget())
+      .subscribe().with(Runnable::run);
+  }
+
+  private void getMatch(final RoutingContext ctx) {
+    reply(ctx, matches.match(UUID.fromString(ctx.pathParam("match"))));
   }
 
   private Uni<HttpServer> listeningHttpServer(final Router router) {
