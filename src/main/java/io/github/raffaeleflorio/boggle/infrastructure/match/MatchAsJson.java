22b639e4479c9434f13ea7f32743b6b296604397
Raffaele Florio <raffaeleflorio@protonmail.com>
2021-09-08 12:07:57 +0200

    MatchAsJson improved and related Test

--- src/main/java/io/github/raffaeleflorio/boggle/infrastructure/match/MatchAsJson.java
+++ src/main/java/io/github/raffaeleflorio/boggle/infrastructure/match/MatchAsJson.java
@@ -1,14 +1,57 @@
 package io.github.raffaeleflorio.boggle.infrastructure.match;
 
+import io.github.raffaeleflorio.boggle.domain.grid.Grid;
 import io.github.raffaeleflorio.boggle.domain.match.Match;
+import io.github.raffaeleflorio.boggle.infrastructure.grid.GridAsJson;
+import io.smallrye.mutiny.operators.AbstractUni;
+import io.smallrye.mutiny.subscription.UniSubscriber;
 import io.vertx.core.json.JsonObject;
 
-public final class MatchAsJson extends JsonObject {
-  public MatchAsJson(final Match<CharSequence> match) {
-    super(
-      new JsonObject()
-        .put("id", match.id())
-        .toBuffer()
-    );
+import java.util.function.Function;
+
+/**
+ * {@link Match} as JSON emitter
+ *
+ * @author Raffaele Florio (raffaeleflorio@protonmail.com)
+ * @since 1.0.0
+ */
+public final class MatchAsJson extends AbstractUni<JsonObject> {
+  /**
+   * Builds the emitter
+   *
+   * @param origin The match
+   * @since 1.0.0
+   */
+  public MatchAsJson(final Match<?> origin) {
+    this(origin, GridAsJson::new);
   }
+
+  /**
+   * Builds the emitter
+   *
+   * @param origin The match
+   * @param gridFn The function to build JSON
+   * @since 1.0.0
+   */
+  MatchAsJson(final Match<?> origin, final Function<Grid<?>, JsonObject> gridFn) {
+    this.origin = origin;
+    this.gridFn = gridFn;
+  }
+
+  @Override
+  public void subscribe(final UniSubscriber<? super JsonObject> uniSubscriber) {
+    origin.grid()
+      .onItem().transform(gridFn)
+      .onItem().transform(grid -> json().put("grid", grid))
+      .subscribe().withSubscriber(uniSubscriber);
+  }
+
+  private JsonObject json() {
+    return new JsonObject()
+      .put("id", origin.description().feature("id").get(0))
+      .put("deadline", origin.description().feature("deadline").get(0));
+  }
+
+  private final Match<?> origin;
+  private final Function<Grid<?>, JsonObject> gridFn;
 }
---
+++ src/test/java/io/github/raffaeleflorio/boggle/infrastructure/match/MatchAsJsonTest.java
@@ -0,0 +1,59 @@
+package io.github.raffaeleflorio.boggle.infrastructure.match;
+
+import io.github.raffaeleflorio.boggle.domain.description.Description;
+import io.github.raffaeleflorio.boggle.domain.grid.Grid;
+import io.github.raffaeleflorio.boggle.domain.match.Match;
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+import static io.github.raffaeleflorio.boggle.hamcrest.IsEmitted.emits;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+class MatchAsJsonTest {
+  @Test
+  void testEmission() {
+    var id = UUID.randomUUID();
+    assertThat(
+      new MatchAsJson(
+        new Match.Fake<>(
+          id,
+          new Description.Fake(
+            Map.of(
+              "id", List.of(id.toString()),
+              "deadline", List.of("a deadline")
+            )
+          ),
+          Map.of(),
+          Map.of(),
+          new Grid.Fake<>(
+            new Description.Fake(
+              Map.of(
+                "lang", List.of("a language"),
+                "layout", List.of("a layout"),
+                "size", List.of("a size")
+              )
+            )
+          )
+        )
+      ).onItem().transform(JsonObject::encode),
+      emits(equalTo(
+          new JsonObject()
+            .put("id", id.toString())
+            .put("deadline", "a deadline")
+            .put("grid", new JsonObject()
+              .put("lang", "a language")
+              .put("size", "a size")
+              .put("layout", new JsonArray().add("a layout"))
+            )
+            .encode()
+        )
+      )
+    );
+  }
+}
