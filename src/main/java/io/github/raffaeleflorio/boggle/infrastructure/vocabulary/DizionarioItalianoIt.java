5ce55638e83718d4cf01401deb5961593eeb6454
Raffaele Florio <raffaeleflorio@protonmail.com>
2021-09-08 13:37:21 +0200

    DizionarioItalianoIt(Test)

---
+++ src/main/java/io/github/raffaeleflorio/boggle/infrastructure/vocabulary/DizionarioItalianoIt.java
@@ -0,0 +1,41 @@
+package io.github.raffaeleflorio.boggle.infrastructure.vocabulary;
+
+import io.github.raffaeleflorio.boggle.domain.dice.Dice;
+import io.github.raffaeleflorio.boggle.domain.vocabulary.Vocabulary;
+import io.smallrye.mutiny.Uni;
+import io.vertx.mutiny.ext.web.client.WebClient;
+
+/**
+ * A {@link Vocabulary} backed by Dizionario Italiano (https://www.dizionario-italiano.it)
+ *
+ * @author Raffaele Florio (raffaeleflorio@protonmail.com)
+ * @since 1.0.0
+ */
+public final class DizionarioItalianoIt implements Vocabulary<CharSequence> {
+  /**
+   * Builds a vocabulary
+   *
+   * @param webClient The webclient
+   * @since 1.0.0
+   */
+  DizionarioItalianoIt(final WebClient webClient) {
+    this(
+      new HttpVocabulary(
+        webClient,
+        "https://www.dizionario-italiano.it/dizionario-italiano.php?parola=%s",
+        "non ha prodotto alcun risultato."
+      )
+    );
+  }
+
+  private DizionarioItalianoIt(final Vocabulary<CharSequence> origin) {
+    this.origin = origin;
+  }
+
+  @Override
+  public Uni<Boolean> contains(final Dice<CharSequence> word) {
+    return origin.contains(word);
+  }
+
+  private final Vocabulary<CharSequence> origin;
+}
---
+++ src/test/java/io/github/raffaeleflorio/boggle/infrastructure/vocabulary/DizionarioItalianoItTest.java
@@ -0,0 +1,61 @@
+package io.github.raffaeleflorio.boggle.infrastructure.vocabulary;
+
+import io.github.raffaeleflorio.boggle.domain.dice.Dice;
+import io.vertx.mutiny.core.Vertx;
+import io.vertx.mutiny.ext.web.client.WebClient;
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+import java.util.List;
+
+import static io.github.raffaeleflorio.boggle.hamcrest.IsEmitted.emits;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.*;
+
+class DizionarioItalianoItTest {
+  @Test
+  void testNotContainsAnEnglishWord() {
+    assertThat(
+      new DizionarioItalianoIt(WebClient.create(Vertx.vertx())).contains(
+        new Dice.Fake<>(
+          List.of("D", "O", "G")
+        )
+      ),
+      emits(
+        equalTo(false),
+        Duration.ofSeconds(5)
+      )
+    );
+  }
+
+  @Test
+  void testContainsAnItalianWord() {
+    assertThat(
+      new DizionarioItalianoIt(WebClient.create(Vertx.vertx())).contains(
+        new Dice.Fake<>(
+          List.of("C", "A", "N", "E")
+        )
+      ),
+      emits(
+        equalTo(true),
+        Duration.ofSeconds(5)
+      )
+    );
+  }
+
+  @Test
+  void testContainsAnotherItalianWord() {
+    assertThat(
+      new DizionarioItalianoIt(WebClient.create(Vertx.vertx())).contains(
+        new Dice.Fake<>(
+          List.of("S", "I", "B", "I", "L", "A", "T", "O")
+        )
+      ),
+      emits(
+        equalTo(true),
+        Duration.ofSeconds(5)
+      )
+    );
+  }
+}
