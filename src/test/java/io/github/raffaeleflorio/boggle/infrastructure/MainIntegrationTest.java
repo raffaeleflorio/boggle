e2bbc3c34d86b9ff7fe72c015009b27af0f29029
Raffaele Florio <raffaeleflorio@protonmail.com>
2021-09-14 17:58:58 +0200

    FIX MainIntegrationTest

--- src/test/java/io/github/raffaeleflorio/boggle/infrastructure/MainIntegrationTest.java
+++ src/test/java/io/github/raffaeleflorio/boggle/infrastructure/MainIntegrationTest.java
@@ -3,10 +3,22 @@ package io.github.raffaeleflorio.boggle.infrastructure;
 import org.junit.jupiter.api.Test;
 
 import static io.github.raffaeleflorio.boggle.infrastructure.Main.main;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class MainIntegrationTest {
   @Test
-  void testNoException() {
-    main(new String[]{});
+  void testExceptionWithoutPortAndHost() {
+    assertThrows(RuntimeException.class, () -> main(new String[]{}));
+  }
+
+  @Test
+  void testExceptionWithInvalidPort() {
+    assertThrows(RuntimeException.class, () -> main(new String[]{"NOPE", "127.0.0.1"}));
+  }
+
+  @Test
+  void testNoExceptionWithPortAndHost() {
+    assertDoesNotThrow(() -> main(new String[]{"0", "127.0.0.1"}));
   }
 }
